9f7ae5584acf69bf594b1d9bb20175b951693552
Ogaga <iyaraogaga69@gmail.com>
2023-02-25 12:46:15 +0100

    Add hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Hero from './components/Hero';
 import MobileNavbar from './components/MobileNavbar';
 import Navbar from './components/Navbar';
 
@@ -6,7 +7,8 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <MobileNavbar/>
+      <MobileNavbar />
+      <Hero/>
     </div>
   );
 }
